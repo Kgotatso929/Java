beebc9c07fd2bd8855b28eb62a450e51618487f3
Kgotatso929 <87439082+Kgotatso929@users.noreply.github.com>
2023-05-31 11:27:17 +0200

    Delete oop/02_reallife/Classes - Homework directory

--- oop/02_reallife/Classes - Homework/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main (String [] args) {
-        School school = new School();
-        school.SchoolName();
-        System.out.println("Education: " + school);
-    }
-}
