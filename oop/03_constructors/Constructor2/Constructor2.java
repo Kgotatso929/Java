1a148d6cdeceab3434ed99ece73b1b915f696054
Kgotatso929 <87439082+Kgotatso929@users.noreply.github.com>
2023-05-31 11:22:13 +0200

    Add files via upload

---
+++ oop/03_constructors/Constructor2/Constructor2.java
@@ -0,0 +1,9 @@
+public class Constructor2 {
+    public Constructor2() {
+        System.out.println("Constructing without a name");
+    }
+
+    public Constructor2 (String name) {
+        System.out.println("Constructing Name: " + name);
+    }
+}
