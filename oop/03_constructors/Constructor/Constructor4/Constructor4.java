b90ead5de7cbc82c24c82f132d97fe88e9ecdcb1
Kgotatso929 <87439082+Kgotatso929@users.noreply.github.com>
2023-06-06 13:29:46 +0200

    Update Constructor4.java

--- oop/03_constructors/Constructor/Constructor4/Constructor4.java
+++ oop/03_constructors/Constructor/Constructor4/Constructor4.java
@@ -1,4 +1,10 @@
-//Answering Question 12, 2.1 Constructors intro lesson 
+//Answering Question 12, of 2.1 Constructors - Intro lesson
+// Fix the program in Question 11.
+// Do not delete the args constructor
+// Do not call the args constructor
+// Call the no args constructor 
+// Only change the constructor2 class
+
 
 public class Constructor4 {
     public Constructor4() {
