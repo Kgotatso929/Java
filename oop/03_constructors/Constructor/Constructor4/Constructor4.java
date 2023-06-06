975fc45402730489a96921902e5818593ae0ab66
Kgotatso929 <87439082+Kgotatso929@users.noreply.github.com>
2023-06-06 10:27:24 +0200

    Add files via upload

---
+++ oop/03_constructors/Constructor/Constructor4/Constructor4.java
@@ -0,0 +1,11 @@
+//Answering Question 12, 2.1 Constructors intro lesson 
+
+public class Constructor4 {
+    public Constructor4() {
+        System.out.println("Constructing without a name");
+    }
+    
+    public Constructor4(String name) {
+        System.out.println("Constructing Name: " + name);
+    }
+}
---
+++ oop/03_constructors/Constructor/Constructor4/ConstructorTest4.java
@@ -0,0 +1,12 @@
+//Answering Question 12, of 2.1 Constructors - Intro lesson
+// Fix the program in Question 11.
+// Do not delete the args constructor
+// Do not call the args constructor
+// Call the no args constructor 
+// Only change the constructor2 class
+
+public class ConstructorTest4 {
+    public static void main(String[] args) {
+        new Constructor4();  // Call the no-args constructor
+    }
+}
