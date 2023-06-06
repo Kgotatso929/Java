34439e004a0a4115fca645831fa87f11a9c49c88
Kgotatso929 <87439082+Kgotatso929@users.noreply.github.com>
2023-06-06 15:22:07 +0200

    Delete oop/04_chaining/Constructor Chaining/Constructor3 directory

--- oop/04_chaining/Constructor Chaining/Constructor3/ConstructorTest3.java
+++
@@ -1,9 +0,0 @@
-/* Answering Question 8 in the Chaining this lessson */
-package Constructor3;
-
-public class ConstructorTest3 {
-    public static void main (String [] args){
-        new Constructor3();
-    }
-    
-}
