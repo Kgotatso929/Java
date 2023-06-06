1334492c8b3b17f1ce9933f55b0fe3d12befd83d
Kgotatso929 <87439082+Kgotatso929@users.noreply.github.com>
2023-06-06 13:25:58 +0200

    Add files via upload

---
+++ oop/03_constructors/Constructor/Constructor3/ConstructorTest3.java
@@ -0,0 +1,10 @@
+//Change the program above such that ConstructorTest2 calls the no args constructor
+
+public class ConstructorTest3 {
+  public static void main (String [] args) {
+  //Call the no args constructor
+    new Constructor3();
+
+  }
+
+}
