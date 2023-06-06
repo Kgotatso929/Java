afcab23b82740ee0f506b537aa0093d7a01820d5
Kgotatso929 <87439082+Kgotatso929@users.noreply.github.com>
2023-06-06 13:27:00 +0200

    Update ConstructorTest3.java

--- oop/03_constructors/Constructor/Constructor3/ConstructorTest3.java
+++ oop/03_constructors/Constructor/Constructor3/ConstructorTest3.java
@@ -1,5 +1,7 @@
 //Change the program above such that ConstructorTest2 calls the no args constructor
-
+//Answering Question 10, of 2.1 Constructors intro 
+/* Change the program above such that constructorTest2 program calls the 
+no args constructor */
 public class ConstructorTest3 {
   public static void main (String [] args) {
   //Call the no args constructor
