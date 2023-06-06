e7f7ed4f5cee91539468a74cadd066fdf3fc0604
Kgotatso929 <87439082+Kgotatso929@users.noreply.github.com>
2023-06-06 23:32:01 +0200

    Delete oop/04_chaining/Constructor Chaining/Constructor5 directory

--- oop/04_chaining/Constructor Chaining/Constructor5/ConstructorTest5.java
+++
@@ -1,7 +0,0 @@
-package Constructor5;
-
-public class ConstructorTest5 {
-    public static void main(String [] args) {
-        new Constructor5("Kgotatso.", "Petlele.", "Software Developer.");
-    }
-}
