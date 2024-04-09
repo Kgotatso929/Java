da1be8b605167ca8e332fec904b904dcd235f2d1
Kgotatso929 <kgotatso.kp@gmail.com>
2024-04-09 13:21:10 +0200

    commit

--- Fundamental_Exams_Postmortems/Halfway_Exam_5/Question22.java
+++ Fundamental_Exams_Postmortems/Halfway_Exam_5/Question22.java
@@ -1,14 +1,15 @@
 public class Question22{
-	public static void main (String [] args){
+	public static void main(String[]args){
 		int k = 9, s = 5;
 		switch(k){
-			default:
-			if(k == 10) {s = s *  2;}
-			else{
-				s  = s + 4;
+			default :
+			if (k ==10) {
+				s = s * 2;
+			}else {
+				s = s + 4;
 				break;
 			}
-		case 7 : s = s + 3;	
+			case 7: s = s + 3;
 		}
 		System.out.println(s);
 	}
---
+++ Fundamental_Exams_Postmortems/Halfway_Exam_5/Question6.java
@@ -0,0 +1,10 @@
+public class Question6{
+	public static void main(String [] args){
+		short x = 10;
+		short y = 3;
+
+		y =+ x;
+		System.out.println(y);
+
+	}
+}
